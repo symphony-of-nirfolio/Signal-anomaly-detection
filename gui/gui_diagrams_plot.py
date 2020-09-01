b23eb1dc3e1c6f7511884619274d5adffc43d162
Nirfiolo <bodyaufml@gmail.com>
2020-09-01 14:26:37 +0300

    Made private some functions

--- gui/gui_diagrams_plot.py
+++ gui/gui_diagrams_plot.py
@@ -16,8 +16,8 @@ def remove_diagrams(vertical_layout: QtWidgets.QVBoxLayout) -> None:
         vertical_layout.itemAt(i).widget().deleteLater()
 
 
-def create_diagram_canvas(main_window: QtWidgets.QMainWindow,
-                          vertical_layout: QtWidgets.QVBoxLayout) -> FigureCanvasQTAgg:
+def _create_diagram_canvas(main_window: QtWidgets.QMainWindow,
+                           vertical_layout: QtWidgets.QVBoxLayout) -> FigureCanvasQTAgg:
     figure = Figure(figsize=(5, 4), dpi=100)
     figure_canvas = FigureCanvasQTAgg(figure)
     figure_canvas.axes = figure.add_subplot(111)
@@ -30,22 +30,22 @@ def create_diagram_canvas(main_window: QtWidgets.QMainWindow,
     return figure_canvas
 
 
-def get_min_form_info(info: Info) -> float:
+def _get_min_form_info(info: Info) -> float:
     return info.min
 
 
-def get_max_form_info(info: Info) -> float:
+def _get_max_form_info(info: Info) -> float:
     return info.max
 
 
-def get_average_from_info(info: Info) -> float:
+def _get_average_from_info(info: Info) -> float:
     return info.average
 
 
-def get_data_points(data: list,
-                    offset: float,
-                    get_data_from_info: Callable[[Info], float],
-                    data_offset=0.0) -> (list, list):
+def _get_data_points(data: list,
+                     offset: float,
+                     get_data_from_info: Callable[[Info], float],
+                     data_offset=0.0) -> (list, list):
     days = []
     observations = []
 
@@ -58,24 +58,24 @@ def get_data_points(data: list,
     return days, observations
 
 
-def get_min_points(data: list, offset: float) -> (list, list):
-    return get_data_points(data, offset, get_min_form_info, data_offset=-273.15)
+def _get_min_points(data: list, offset: float) -> (list, list):
+    return _get_data_points(data, offset, _get_min_form_info, data_offset=-273.15)
 
 
-def get_max_points(data: list, offset: float) -> (list, list):
-    return get_data_points(data, offset, get_max_form_info, data_offset=-273.15)
+def _get_max_points(data: list, offset: float) -> (list, list):
+    return _get_data_points(data, offset, _get_max_form_info, data_offset=-273.15)
 
 
-def get_average_points(data: list, offset: float) -> (list, list):
-    return get_data_points(data, offset, get_average_from_info, data_offset=-273.15)
+def _get_average_points(data: list, offset: float) -> (list, list):
+    return _get_data_points(data, offset, _get_average_from_info, data_offset=-273.15)
 
 
-def get_points_by_month(data: list,
-                        get_points: Callable[[list, float], Tuple[list, list]]) -> (list, list):
+def _get_points_by_month(data: list,
+                         get_points: Callable[[list, float], Tuple[list, list]]) -> (list, list):
     return get_points(data, 0)
 
 
-def calculate_offset(year: str, month: str) -> int:
+def _calculate_offset(year: str, month: str) -> int:
     month_begin = date(int(year), int(month), 1)
     next_month_begin = date(
         int(year) if month != "12" else int(year) + 1,
@@ -86,8 +86,8 @@ def calculate_offset(year: str, month: str) -> int:
     return days
 
 
-def get_points_by_several_months(data: dict,
-                                 get_points: Callable[[list, float], Tuple[list, list]]) -> (list, list):
+def _get_points_by_several_months(data: dict,
+                                  get_points: Callable[[list, float], Tuple[list, list]]) -> (list, list):
     days_all = []
     observations_all = []
 
@@ -97,13 +97,13 @@ def get_points_by_several_months(data: dict,
         days_all += days
         observations_all += observations
 
-        offset += calculate_offset(data[key][1], key)
+        offset += _calculate_offset(data[key][1], key)
 
     return days_all, observations_all
 
 
-def get_points_by_all_data(data: dict,
-                           get_points: Callable[[list, float], Tuple[list, list]]) -> (list, list):
+def _get_points_by_all_data(data: dict,
+                            get_points: Callable[[list, float], Tuple[list, list]]) -> (list, list):
     days_all = []
     observations_all = []
 
@@ -116,20 +116,20 @@ def get_points_by_all_data(data: dict,
             days_all += days
             observations_all += observations
 
-            offset += calculate_offset(current_data[key][1], key)
+            offset += _calculate_offset(current_data[key][1], key)
 
     return days_all, observations_all
 
 
-def show_diagram_by_points_function(main_window: QtWidgets.QMainWindow,
-                                    vertical_layout: QtWidgets.QVBoxLayout,
-                                    data: Any,
-                                    get_points:
-                                        Callable[[Any, Callable[[list, float], Tuple[list, list]]], Tuple[list, list]],
-                                    need_min_temperature=False,
-                                    need_max_temperature=False,
-                                    need_average_temperature=False) -> None:
-    figure_canvas = create_diagram_canvas(main_window, vertical_layout)
+def _show_diagram_by_points_function(main_window: QtWidgets.QMainWindow,
+                                     vertical_layout: QtWidgets.QVBoxLayout,
+                                     data: Any,
+                                     get_points:
+                                     Callable[[Any, Callable[[list, float], Tuple[list, list]]], Tuple[list, list]],
+                                     need_min_temperature=False,
+                                     need_max_temperature=False,
+                                     need_average_temperature=False) -> None:
+    figure_canvas = _create_diagram_canvas(main_window, vertical_layout)
 
     size = 0
     days = []
@@ -138,7 +138,7 @@ def show_diagram_by_points_function(main_window: QtWidgets.QMainWindow,
     labels = []
 
     if need_max_temperature:
-        days_form_max, max_temperature = get_points(data, get_max_points)
+        days_form_max, max_temperature = get_points(data, _get_max_points)
         days.append(days_form_max)
         observations.append(max_temperature)
         colors.append('r')
@@ -146,7 +146,7 @@ def show_diagram_by_points_function(main_window: QtWidgets.QMainWindow,
         size += 1
 
     if need_average_temperature:
-        days_form_average, average_temperature = get_points(data, get_average_points)
+        days_form_average, average_temperature = get_points(data, _get_average_points)
         days.append(days_form_average)
         observations.append(average_temperature)
         colors.append('m')
@@ -154,7 +154,7 @@ def show_diagram_by_points_function(main_window: QtWidgets.QMainWindow,
         size += 1
 
     if need_min_temperature:
-        days_form_min, min_temperature = get_points(data, get_min_points)
+        days_form_min, min_temperature = get_points(data, _get_min_points)
         days.append(days_form_min)
         observations.append(min_temperature)
         colors.append('y')
@@ -172,10 +172,10 @@ def show_diagram_by_month(main_window: QtWidgets.QMainWindow,
                           need_min_temperature=False,
                           need_max_temperature=False,
                           need_average_temperature=False) -> None:
-    show_diagram_by_points_function(main_window, vertical_layout, data, get_points_by_month,
-                                    need_min_temperature=need_min_temperature,
-                                    need_max_temperature=need_max_temperature,
-                                    need_average_temperature=need_average_temperature)
+    _show_diagram_by_points_function(main_window, vertical_layout, data, _get_points_by_month,
+                                     need_min_temperature=need_min_temperature,
+                                     need_max_temperature=need_max_temperature,
+                                     need_average_temperature=need_average_temperature)
 
 
 def show_diagram_by_several_months(main_window: QtWidgets.QMainWindow,
@@ -184,10 +184,10 @@ def show_diagram_by_several_months(main_window: QtWidgets.QMainWindow,
                                    need_min_temperature=False,
                                    need_max_temperature=False,
                                    need_average_temperature=False) -> None:
-    show_diagram_by_points_function(main_window, vertical_layout, data, get_points_by_several_months,
-                                    need_min_temperature=need_min_temperature,
-                                    need_max_temperature=need_max_temperature,
-                                    need_average_temperature=need_average_temperature)
+    _show_diagram_by_points_function(main_window, vertical_layout, data, _get_points_by_several_months,
+                                     need_min_temperature=need_min_temperature,
+                                     need_max_temperature=need_max_temperature,
+                                     need_average_temperature=need_average_temperature)
 
 
 def show_diagram_by_several_all_data(main_window: QtWidgets.QMainWindow,
@@ -196,7 +196,7 @@ def show_diagram_by_several_all_data(main_window: QtWidgets.QMainWindow,
                                      need_min_temperature=False,
                                      need_max_temperature=False,
                                      need_average_temperature=False) -> None:
-    show_diagram_by_points_function(main_window, vertical_layout, data, get_points_by_all_data,
-                                    need_min_temperature=need_min_temperature,
-                                    need_max_temperature=need_max_temperature,
-                                    need_average_temperature=need_average_temperature)
+    _show_diagram_by_points_function(main_window, vertical_layout, data, _get_points_by_all_data,
+                                     need_min_temperature=need_min_temperature,
+                                     need_max_temperature=need_max_temperature,
+                                     need_average_temperature=need_average_temperature)
